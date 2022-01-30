9cbbbd3f8f2f11a94dbba1c49fb01cfec16903f3
tachapong <tachapong@perfenterprise.com>
2022-01-30 10:40:30 +0700

    Implement setup plan flow.

--- src/app/setup-plan-config/setup-plan-config.page.ts
+++ src/app/setup-plan-config/setup-plan-config.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -8,12 +9,64 @@ import { ModalController } from '@ionic/angular';
 })
 export class SetupPlanConfigPage implements OnInit {
 
-  constructor(private dlg: ModalController) { }
+  @Input() id: string;
+  @Input() displayName: string;
+  @Input() payPerMonth: string;
+  @Input() payPerYear: string;
+  @Input() feePerMonth: string;
+  @Input() feePerYear: string;
 
-  ngOnInit() {
+  public fg: FormGroup;
+  public isFirstTime: boolean = true;
+
+  constructor(private dlg: ModalController, private fb: FormBuilder) {
+    this.fg = this.fb.group({
+      'id': null,
+      'displayName': [null, Validators.required],
+      'payPerMonth': [50, Validators.required],
+      'payPerYear': [600, Validators.required],
+      'feePerMonth': [9, Validators.required],
+      'feePerYear': [108, Validators.required],
+    });
+
+
   }
 
-  dismiss() {
-    this.dlg.dismiss();
+  ngOnInit() {
+    console.log(this.id);
+
+    this.fg.get('id').setValue(this.id);
+    let isDataValid =
+      this.displayName &&
+      this.payPerMonth &&
+      this.payPerYear &&
+      this.feePerMonth &&
+      this.feePerYear;
+    if (isDataValid) {
+      this.fg.get('displayName').setValue(this.displayName);
+      this.fg.get('payPerMonth').setValue(this.payPerMonth);
+      this.fg.get('payPerYear').setValue(this.payPerYear);
+      this.fg.get('feePerMonth').setValue(this.feePerMonth);
+      this.fg.get('feePerYear').setValue(this.feePerYear);
+    }
+  }
+
+  onPayPerMonthUpdate(event) {
+    this.fg.get('payPerYear').setValue(event.target.value * 12);
+  }
+
+  onFeePerMonthUpdate(event) {
+    this.fg.get('feePerYear').setValue(event.target.value * 12);
+  }
+
+  onDismiss() { this.dlg.dismiss(); }
+  onSubmit() {
+    if (this.isFirstTime) {
+      this.isFirstTime = false;
+    }
+    
+    if (this.fg.valid) {
+      this.dlg.dismiss(this.fg.value);
+    }
   }
 }
--- src/app/setup-plan/setup-plan.page.ts
+++ src/app/setup-plan/setup-plan.page.ts
@@ -9,17 +9,54 @@ import { SetupPlanConfigPage } from '../setup-plan-config/setup-plan-config.page
 })
 export class SetupPlanPage implements OnInit {
 
+  public data = [
+    { "id": 1, "displayName": "ขั้นต่ำ", "payPerMonth": 50, "payPerYear": 600, "feePerMonth": 9, "feePerYear": 108 },
+    { "id": 2, "displayName": "บ้านจัดสรร", "payPerMonth": 100, "payPerYear": 1200, "feePerMonth": 12, "feePerYear": 144 },
+  ];
+
   constructor(private dlg: ModalController) { }
 
   ngOnInit() {
   }
 
-  async openModal(){
+  async openAddPayment() {
     const modal = await this.dlg.create({
       component: SetupPlanConfigPage,
-      cssClass: 'dialog-modal-dlg-note',
+      componentProps: { "id": this.data.length + 1 }
     });
-    return await modal.present();
+    await modal.present();
+
+    modal.onDidDismiss().then(it => {
+      if (it && it.data) {
+        this.data.push(it.data);
+      }
+    });
   }
+
+
+  async openEditPayment(item: any) {
+    const modal = await this.dlg.create({
+      component: SetupPlanConfigPage,
+      componentProps: {
+        "id": item.id,
+        "displayName": item.displayName,
+        "payPerMonth": item.payPerMonth,
+        "payPerYear": item.payPerYear,
+        "feePerMonth": item.feePerMonth,
+        "feePerYear": item.feePerYear,
+      }
+    });
+    await modal.present();
+
+    modal.onDidDismiss().then(it => {
+      if (it && it.data) {
+        let result = this.data.find(x => x.id == it.data.id);
+        let spliceIndex = this.data.indexOf(result);
+        this.data[spliceIndex] = it.data;
+      }
+    });
+  }
+
+
 
 }
