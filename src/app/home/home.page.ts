e26179bc9adba4339232c03ad97828f6fb4c8a8d
tachapong <tachapong@perfenterprise.com>
2022-03-09 15:11:19 +0700

    Update chart images. Update UI.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -8,12 +8,31 @@ import { MenuController } from '@ionic/angular';
 })
 export class HomePage implements OnInit {
 
+  private currencyText: string = "ดูแบบเงิน"
+  private householdText: string = "ดูแบบครัวเรือน"
+  private isViewCurrencyChart: boolean = true;
+
+  public buttonText: string = this.householdText;
+  public chartUrl: string = "/assets/imgs/overall-currency.png";
+
   constructor(private menu: MenuController) {
     this.menu.enable(true);
 
   }
 
   ngOnInit() {
+  }
+
+  onSwitchChart() {
+    if (this.isViewCurrencyChart) {
+      this.buttonText = this.currencyText;
+      this.chartUrl = "/assets/imgs/overall-household.png";
+    }
+    else {
+      this.buttonText = this.householdText;
+      this.chartUrl = "/assets/imgs/overall-currency.png";
+    }
+    this.isViewCurrencyChart = !this.isViewCurrencyChart;
   }
 
 }
--- src/app/payment-bargraph/payment-bargraph.page.ts
+++ src/app/payment-bargraph/payment-bargraph.page.ts
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentBargraphPage implements OnInit {
 
+  private currencyText: string = "ดูแบบเงิน"
+  private householdText: string = "ดูแบบครัวเรือน"
+  private isViewCurrencyChart: boolean = true;
+
+  public buttonText: string = this.householdText;
+  public chartUrl: string = "/assets/imgs/annual-payment-currency.png";
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSwitchChart() {
+    if (this.isViewCurrencyChart) {
+      this.buttonText = this.currencyText;
+      this.chartUrl = "/assets/imgs/annual-payment-household.png";
+    }
+    else {
+      this.buttonText = this.householdText;
+      this.chartUrl = "/assets/imgs/annual-payment-currency.png";
+    }
+    this.isViewCurrencyChart = !this.isViewCurrencyChart;
   }
 
 }
