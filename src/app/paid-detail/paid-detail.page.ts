feedbf971c16168e508abd946eb3824f02a30578
tachapong <tachapong@perfenterprise.com>
2022-03-09 18:07:49 +0700

    Implement data from google sheet.

--- src/app/paid-detail/paid-detail.page.ts
+++ src/app/paid-detail/paid-detail.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AddressInfo, DataService } from 'src/services/data.service';
 
 @Component({
   selector: 'app-paid-detail',
@@ -7,9 +8,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaidDetailPage implements OnInit {
 
-  constructor() { }
+  private totalData: AddressInfo[];
+  
+  public totalAmountData: number;
+  public dispalyData: AddressInfo[] = new Array<AddressInfo>();
+
+  constructor(private dataSvc: DataService) {
+
+    this.totalData = dataSvc.addresses.
+      filter(it => it.paymentInfo.overdueDate == null &&
+        it.paymentInfo.overrideDateAmount == null &&
+        it.paymentInfo.overdueAmount == null);
+
+    this.totalAmountData = this.totalData.length;
+    this.dispalyData = this.totalData.splice(0, 20);
+  }
 
   ngOnInit() {
+  }
+
+  loadData(event) {
+    setTimeout(() => {
+
+      let insertData = this.totalData.splice(0, 10);
+      insertData.forEach(it => this.dispalyData.push(it));
+
+      if (this.totalData.length < 1) { event.target.disabled = true; }
+      event.target.complete();
+    }, 1000);
   }
 
 }
