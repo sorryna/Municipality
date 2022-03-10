6d32353ffe7a48e83a6143be4d87abce46388f2f
tachapong <tachapong@perfenterprise.com>
2022-03-10 19:00:49 +0700

    Implement payment flows. Connect payment flow with report flow.

--- src/app/qroption/qroption.page.ts
+++ src/app/qroption/qroption.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+import { DataService } from 'src/services/data.service';
 
 @Component({
   selector: 'app-qroption',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QROptionPage implements OnInit {
 
-  constructor() { }
+  public navigations: any[] = [];
+  public data: any;
+
+  constructor(private dataSvc: DataService, private router: Router, private route: ActivatedRoute) {
+    if (this.route.queryParams) {
+      this.route.queryParams.subscribe(params => {
+        let id = params["id"];
+        if (id) {
+          this.data = dataSvc.addresses.find(it => it.id == id)
+        }
+
+        let navigation = params["navigation"];
+        if (navigation) {
+          this.navigations = JSON.parse(navigation).filter(it => it.url != "/qroption");
+        }
+      });
+    }
+  }
 
   ngOnInit() {
+  }
+
+  onSelectPay(value: number) {
+    this.navigations.push({ url: "/qroption", text: "ช่องทางการชำระเงิน" });
+    let param: NavigationExtras = { queryParams: { id: this.data.id, amount: value, navigation: JSON.stringify(this.navigations) } };
+    this.router.navigate(['/qrgenerate'], param);
   }
 
 }
